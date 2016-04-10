964a152e303801a39ac42f9fe0a8f0f2c9597579
Alberto Saavedra <saavedraloko@hotmail.com>
2016-04-09 21:48:27 -0500

    Segundo Proyecto

--- FirstTest/FirstTest/FirstTest/FirstTest/App.cs
+++ FirstTest/FirstTest/FirstTest/FirstTest/App.cs
@@ -26,7 +26,7 @@ namespace FirstTest
                     Children = {
                         new Label {
                             XAlign = TextAlignment.Center,
-                            Text = "Welcome to Xamarin Forms!"
+                            Text = "Primer Proyecto Xamarin!"
                         },
                         btn 
                     }
